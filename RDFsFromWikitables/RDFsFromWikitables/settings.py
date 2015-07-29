593d0b64d676fe06730431fe6837976862798684
Alexander Immer <alexander.immer@yahoo.com>
2015-07-29 18:17:58 +0200

    add local password

--- RDFsFromWikitables/RDFsFromWikitables/settings.py
+++ RDFsFromWikitables/RDFsFromWikitables/settings.py
@@ -81,6 +81,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
         'NAME': 'rdfs',
         'USER': 'postgres',
+        'PASSWORD': 'manager',
         'HOST': 'localhost',
         'PORT': '5432',
     }

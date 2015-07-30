8b4be3f56c4909e125ef868b26e5d0b93ab54358
Alexander Immer <alexander.immer@yahoo.com>
2015-07-31 00:52:43 +0200

    lock db for filters

--- RDFsFromWikitables/RDFs/management/commands/extractPages.py
+++ RDFsFromWikitables/RDFs/management/commands/extractPages.py
@@ -8,7 +8,7 @@ from wikitables.page import Page as wikipage
 
 import time
 
-THREAD_MAX = 32
+THREAD_MAX = 64
 
 num_threads = 0
 lock = allocate_lock()
@@ -46,12 +46,15 @@ def generateRDFsFor(title):
 
     print("Title: "+ str(title).strip())
 
+    db_lock.acquire()
     if Page.objects.filter(title=str(title)):
+        db_lock.release()
         print(str(title).strip() + " already extracted.")
         lock.acquire()
         num_threads -= 1
         lock.release()
         return
+    db_lock.release()
 
     try:
         wpage = None
@@ -62,7 +65,11 @@ def generateRDFsFor(title):
                     "Couldn\'t find wikipedia page with this title\n" +
                     "FAILED for page with title: " + str(title).strip() + "\n" +
                     "------------------------------")
+
+        acquired = False
+        db_lock.acquire()
         if wpage and not Page.objects.filter(link=(str(wpage.url))):
+            db_lock.release()
             pg = Page(title=str(title), link=str(wpage.url), tables=len(wpage.tables))
             pg.save()
             if wpage.hasTable:
@@ -74,6 +81,7 @@ def generateRDFsFor(title):
                     for rdf in rdfs:
                         # save the data:
                         db_lock.acquire()
+                        acquired = True
                         # exclude errors like empty subject, predicate or object and
                         # errors such as subject != resource
                         if rdf[0] and rdf[1] and rdf[2] and ('/resource/' in rdf[0]):
@@ -82,9 +90,11 @@ def generateRDFsFor(title):
                                     subject_is_tablekey=rdf[5], object_is_tablekey=rdf[6],
                                     table_number=i, number_of_tablerows=rdf[7]).save()
                         db_lock.release()
+                        acquired = False
             else:
                 print('Page with title \''+str(title).strip()+'\' has no tables')
         else:
+            db_lock.release()
             print('Page with title \'' + str(title).strip() + '\' has been extracted already')
     except Exception as inst:
         print("\n------------------------------\n" +
@@ -96,6 +106,9 @@ def generateRDFsFor(title):
         print("\n------------------------------\n" +
                 "Unknown error appeared for page with title: " + str(title).strip() + "\n" +
                 "------------------------------")
+
+    if acquired:
+        db_lock.release()
 
     lock.acquire()
     num_threads -= 1

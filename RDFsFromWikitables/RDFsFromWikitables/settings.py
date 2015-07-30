e58a7e7c8b18b02e1907da2d1434241b807500db
Alexander Immer <alexander.immer@yahoo.com>
2015-07-30 16:23:22 +0200

    change DB

--- RDFsFromWikitables/RDFsFromWikitables/settings.py
+++ RDFsFromWikitables/RDFsFromWikitables/settings.py
@@ -79,7 +79,7 @@ WSGI_APPLICATION = 'RDFsFromWikitables.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'rdfs',
+        'NAME': 'wiki_rdfs',
         'USER': 'postgres',
         'PASSWORD': 'manager',
         'HOST': 'localhost',

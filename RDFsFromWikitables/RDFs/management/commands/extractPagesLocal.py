207f4b65ac94dc240dffe3b977d648820e1a2b0e
Thomas Kellermeier <t-kellermeier@hotmail.de>
2015-08-06 20:17:40 +0200

    Extract pages local corrected

--- RDFsFromWikitables/RDFs/management/commands/extractPagesLocal.py
+++ RDFsFromWikitables/RDFs/management/commands/extractPagesLocal.py
@@ -49,36 +49,43 @@ class Command(BaseCommand):
                 if num_threads < THREAD_MAX:
                     break
                 lock.release()
-            generateRDFsFor(pg)
+            lock.release()
+            start_new_thread(generateRDFsFor,(pg,))
 
 
 def generateRDFsFor(pg):
     global num_threads, lock, db_lock
 
     loc_pg = LocalPage(pg.title, pg.html, pg.link)
-    print(loc_pg.summary)
-    print(loc_pg.categories)
+    #print(loc_pg.summary)
+    #print(loc_pg.categories)
     title = pg.title
 
     index = 0
-    for table in loc_pg.tables:
-        tb = Table(page=pg, table_number=index, number_of_tablerows=len(table.rows))
-        tb.save()
+    if loc_pg.hasTable():
+        for table in loc_pg.tables:
+            if not Table.objects.filter(page=pg, table_number=index):
+                tb = Table(page=pg, table_number=index, number_of_tablerows=len(table.rows))
+                tb.save()
 
-        rdfs = table.generateRDFs()
-        print(str(len(rdfs)) + ' new RDFs generated for table ' + str(title).strip())
-        for rdf in rdfs:
-            db_lock.acquire()
-            # exclude errors like empty subject, predicate or object and
-            # errors such as subject != resource
-            if rdf[0] and rdf[1] and rdf[2] and ('/resource/' in rdf[0]):
-                RDF(table=tb, rdf_subject=rdf[0], rdf_predicate=rdf[1], rdf_object=rdf[2],
-                        object_column_name=rdf[3], relative_occurency=rdf[4],
-                        subject_is_tablekey=rdf[5], object_is_tablekey=rdf[6],
-                        table_number=index, number_of_tablerows=rdf[7]).save()
-
-            db_lock.release()
-            acquired = False
+                rdfs = table.generateRDFs()
+                print(str(len(rdfs)) + ' new RDFs generated for table ' + str(title).strip())
+                for rdf in rdfs:
+                    db_lock.acquire()
+                    # exclude errors like empty subject, predicate or object and
+                    # errors such as subject != resource
+                    if rdf[0] and rdf[1] and rdf[2] and ('/resource/' in rdf[0]):
+                        RDF(table=tb, rdf_subject=rdf[0], rdf_predicate=rdf[1], rdf_object=rdf[2],
+                                object_column_name=rdf[3], relative_occurency=rdf[4],
+                                subject_is_tablekey=rdf[5], object_is_tablekey=rdf[6]).save()
+                        print('RDF for Table generated')
+                    db_lock.release()
+                    acquired = False
+            else:
+                print('Table ' + str(table_number) + ' from page with title \'' + str(pg.title).strip() + '\' has been extracted already')
+    else:
+        print('Page has no tables')
+        pg.delete()
 
     lock.acquire()
     num_threads -= 1

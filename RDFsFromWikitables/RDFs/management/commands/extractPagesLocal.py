647cb378360e4df3cba5cf8d294daaeefdaf3ae1
Alexander Immer <alexander.immer@yahoo.com>
2015-08-20 20:51:35 +0200

    extract pages local (tested)

--- RDFsFromWikitables/RDFs/management/commands/extractPagesLocal.py
+++ RDFsFromWikitables/RDFs/management/commands/extractPagesLocal.py
@@ -9,7 +9,7 @@ import wikipedia
 
 import time
 
-THREAD_MAX = 8
+THREAD_MAX = 16
 
 num_threads = 0
 lock = allocate_lock()
@@ -39,33 +39,32 @@ def generateRDFsFor(pg):
     loc_pg = LocalPage(pg.title, pg.html, pg.link)
     title = pg.title
 
-    if loc_pg.hasTable():
-        index = 0
-        for table in loc_pg.tables:
-            try:
-                tb = Table(page=pg, table_number=index, number_of_tablerows=len(table.rows))
-                tb.save()
+    index = 0
+    for table in loc_pg.tables:
+        try:
+            tb = Table(page=pg, table_number=index, number_of_tablerows=len(table.rows))
+            tb.save()
 
-                rdfs = table.generateRDFs()
-                print(str(len(rdfs)) + ' new RDFs generated for table')
+            rdfs = table.generateRDFs()
+            print(str(len(rdfs)) + ' new RDFs generated for table')
 
-                for rdf in rdfs:
-                    db_lock.acquire()
-                    # exclude errors like empty subject, predicate or object and
-                    # errors such as subject != resource
-                    # remove wikiPageWikiLink as its not on public SPARQL Endpoint
-                    if rdf[0] and rdf[1] and rdf[2] and ('/resource/' in rdf[0]) and not ('http://dbpedia.org/ontology/wikiPageWikiLink' in rdf[1]):
-                        print(rdf)
-                        RDF(table=tb, rdf_subject=rdf[0], rdf_predicate=rdf[1], rdf_object=rdf[2],
-                                object_column_name=rdf[3], relative_occurency=rdf[4],
-                                subject_is_tablekey=rdf[5], object_is_tablekey=rdf[6]).save()
-                    db_lock.release()
-            except:
-                print('Unexpected error occurred during RDF-generating')
+            for rdf in rdfs:
+                db_lock.acquire()
+                # exclude errors like empty subject, predicate or object and
+                # errors such as subject != resource
+                # remove wikiPageWikiLink as its not on public SPARQL Endpoint
+                if rdf[0] and rdf[1] and rdf[2] and ('/resource/' in rdf[0]) and not ('http://dbpedia.org/ontology/wikiPageWikiLink' in rdf[1]):
+                    RDF(table=tb, rdf_subject=rdf[0], rdf_predicate=rdf[1], rdf_object=rdf[2],
+                            object_column_name=rdf[3], relative_occurency=rdf[4],
+                            subject_is_tablekey=rdf[5], object_is_tablekey=rdf[6]).save()
+                db_lock.release()
+        except:
+            print('Unexpected error occurred during RDF-generating')
 
-            index += 1
-    else:
-        print('Page has no tables')
+        index += 1
+    if index == 0:
+        print('page has no tables')
+
     lock.acquire()
     num_threads -= 1
     lock.release()

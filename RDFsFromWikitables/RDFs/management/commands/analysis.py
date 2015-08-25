27dbe14d1f01ef4acdfa0ac50694d26a93c35b9c
Alexander Immer <alexander.immer@yahoo.com>
2015-08-25 11:11:37 -0700

    quick analysis

--- RDFsFromWikitables/RDFs/management/commands/analysis.py
+++ RDFsFromWikitables/RDFs/management/commands/analysis.py
@@ -3,17 +3,18 @@ from RDFs.models import RDF, Page, Table
 
 class Command(BaseCommand):
     def handle(self, *args, **options):
-        # start scripts/methods at this point
+        analyseExtent()
 
 
 def analyseExtent():
     tables_size = Table.objects.all().count()
     pages_size = Page.objects.all().count()
-    # can be found out differently
-    pages_with_tables = Page.objects.filter(html_icontains='wikitable').count()
 
     print('Amount of Tables: ' + str(tables_size))
     print('Amount of Pages: ' str(pages_size))
+
+    pages_with_tables = Page.objects.filter(html_icontains='wikitable').count()
+
     print('Amount of Pages with tables: ' + str(pages_with_tables))
     print('Tables per Page: ' + str(tables_size/pages_size))
     print('Tables per Page containing at least one table: ' + str(tables_size/pages_with_tables))
